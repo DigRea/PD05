52ec7b21077761be23cd52c5857b9a6c01e07613
DigRea <146025236+DigRea@users.noreply.github.com>
2023-12-04 17:12:18 +0300

    Initial

---
+++ filemanager.py
@@ -0,0 +1,36 @@
+while True:
+    print('1 - Создать папку\t\t\t\t', ' 7 - Просмотр информации об ОС')
+    print('2 - Удалить файл/папку\t\t\t', ' 8 - Создатель программы')
+    print('3 - Просмотр рабочей директории\t', ' 9 - Играть в викторину')
+    print('4 - Просмотр рабочей директории\t', '10 - Мой банковский счёт')
+    print('5 - Просмотреть только папки\t', '11 - ')
+    print('6 - Просмотреть только файлы\t', '12 - Выход')
+    print()
+    choice = input('Выберите пункт меню: ')
+    if choice == '1':
+        pass
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass
+    elif choice == '4':
+        pass
+    elif choice == '5':
+        pass
+    elif choice == '6':
+        pass
+    elif choice == '7':
+        pass
+    elif choice == '8':
+        pass
+    elif choice == '9':
+        pass
+    elif choice == '10':
+        pass
+    elif choice == '11':
+        pass
+    elif choice == '12':
+        break
+    else:
+        print('Неверный пункт меню')
+        print()
